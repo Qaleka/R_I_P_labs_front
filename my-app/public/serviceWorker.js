403ca0fa81550c90c1701832d8cd7c3ba755074a
Qaleka <91965947+Qaleka@users.noreply.github.com>
2023-11-14 23:17:30 +0300

    some mock changes

--- my-app/public/serviceWorker.js
+++ my-app/public/serviceWorker.js
@@ -1,42 +1,38 @@
-const timeout = 4000;
+const timeout = 400;
 
-const containers = {
-  "draft_transportation": null,
-  "containers": [
-    {
-      "uuid": "a20163ce-7be5-46ec-a50f-a313476b2bd1",
-      "marking": "CCCU6543210",
-      "type": "Стандартный 20-ти футовый контейнер",
-      "length": 5905,
-      "height": 2381,
-      "width": 2350,
-      "image_url": "localhost:9000/images/a20163ce-7be5-46ec-a50f-a313476b2bd1.jpg",
-      "cargo": "Зерно",
-      "weight": 15000
-    },
-    {
-      "uuid": "8f157a95-dad1-43e0-9372-93b51de06163",
-      "marking": "DDDU6543210",
-      "type": "20 футовый контейнер увеличенной высоты",
-      "length": 5905,
-      "height": 2596,
-      "width": 2350,
-      "image_url": "localhost:9000/images/8f157a95-dad1-43e0-9372-93b51de06163.jpg",
-      "cargo": "Фрукты",
-      "weight": 13000
-    },
-    {
-      "uuid": "07d0cbdc-8e0f-4308-a7aa-11976ee6e5b2",
-      "marking": "BBBU6543210",
-      "type": "Стандартный 40-ка футовый контейнер",
-      "length": 12045,
-      "height": 2381,
-      "width": 2350,
-      "image_url": "localhost:9000/images/07d0cbdc-8e0f-4308-a7aa-11976ee6e5b2.jpg",
-      "cargo": "Телевизоры",
-      "weight": 19000
-    }
-  ]
+const draft_notifications = null
+const recipients = {//change data
+  "a20163ce-7be5-46ec-a50f-a313476b2bd1": {
+    "uuid": "a20163ce-7be5-46ec-a50f-a313476b2bd1",
+    "fio": "",
+    "email": " ",
+    "age": ,
+    "image_url": "",
+    "adress": "",
+
+  },
+  "8f157a95-dad1-43e0-9372-93b51de06163": {
+    "uuid": "8f157a95-dad1-43e0-9372-93b51de06163",
+    "marking": "DDDU6543210",
+    "type": "20 футовый контейнер увеличенной высоты",
+    "length": 5905,
+    "height": 2596,
+    "width": 2350,
+    "image_url": "localhost:9000/images/8f157a95-dad1-43e0-9372-93b51de06163.jpg",
+    "cargo": "Фрукты",
+    "weight": 13000
+  },
+  "07d0cbdc-8e0f-4308-a7aa-11976ee6e5b2": {
+    "uuid": "07d0cbdc-8e0f-4308-a7aa-11976ee6e5b2",
+    "marking": "BBBU6543210",
+    "type": "Стандартный 40-ка футовый контейнер",
+    "length": 12045,
+    "height": 2381,
+    "width": 2350,
+    "image_url": "localhost:9000/images/07d0cbdc-8e0f-4308-a7aa-11976ee6e5b2.jpg",
+    "cargo": "Телевизоры",
+    "weight": 19000
+  }
 }
 
 function fromNetwork(request, timeout) {
@@ -58,13 +54,44 @@ function fromNetwork(request, timeout) {
 self.addEventListener('fetch', (event) => {
   const requestURL = new URL(event.request.url);
 
-  if (requestURL.pathname.startsWith('/api/containers')) {
-    event.respondWith(fromNetwork(event.request, timeout)
+  if (requestURL.pathname.startsWith('/api/recipients')) {
+    event.respondWith(
+      fromNetwork(event.request, timeout)
         .catch((err) => {
           console.log(`Error caught: ${err.message}`);
-          return new Response(JSON.stringify(containers), {
-            headers: { 'Content-Type': 'application/json; charset=utf-8' },
-          });
+          const recipientIdMatch = requestURL.pathname.match(/^\/api\/recipients\/([^/]+)$/);
+
+          if (recipientIdMatch) {
+            const recipientId = recipientIdMatch[1];
+            return new Response(JSON.stringify(recipients[recipientId]), {
+              headers: { 'Content-Type': 'application/json; charset=utf-8' },
+            });
+          } else {
+            return new Response(JSON.stringify({ draft_notification, recipients: Object.values(recipients) }), {
+              headers: { 'Content-Type': 'application/json; charset=utf-8' },
+            });
+          }
+        })
+    );
+  } else if (requestURL.pathname.startsWith('/images')) {
+    event.respondWith(
+      fromNetwork(event.request, timeout)
+        .catch((_) => {
+          console.log(`Failed to load image`);
+          return fetch('/placeholder2.jpeg')//change image
+            .then((response) => {
+              return new Response(response.body, {
+                status: response.status,
+                statusText: response.statusText,
+                headers: new Headers(response.headers),
+              });
+            })
+            .catch((_) => {
+              return new Response('Failed to load image', {
+                status: 500,
+                headers: { 'Content-Type': 'text/plain' },
+              });
+            });
         })
     );
 
