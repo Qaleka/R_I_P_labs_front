2855b31f8eee4844dbff6a0d541497dbb5bedc62
Qaleka <91965947+Qaleka@users.noreply.github.com>
2023-12-16 10:58:12 +0300

    Updates

--- my-app/src/pages/AllNotifications.tsx
+++
@@ -1,8 +0,0 @@
-const AllNotifications = () => {
-   
-    return (
-        <h3 className="text-center">Для просмотра этой страницы необходимо авторизоваться</h3>
-    )
-}
-
-export { AllNotifications }
